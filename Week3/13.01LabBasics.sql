022c68fd17dce90d5f11b862746b53f2e13f448e
Pedro10Rocha <120037010+Pedro10Rocha@users.noreply.github.com>
2023-01-23 17:49:17 +0100

    update

--- Week3/13.01LabBasics.sql
+++ Week3/13.01LabBasics.sql
@@ -26,3 +26,19 @@ select amount from `order` where account_to = 30067122;
 #12. In the trans table, show the trans_id, date, type and amount of the 10 first transactions from account_id 793 in chronological order, from newest to oldest.
 select trans_id, `date`, `type`, amount from trans where account_id = 793 order by `date` desc limit 10;
 #13. In the client table, of all districts with a district_id lower than 10, how many clients are from each district_id? Show the results sorted by the district_id in ascending order.
+select district_id, count	(client_id) from `client` where district_id < 10 group by district_id order by district_id;
+#14. In the card table, how many cards exist for each type? Rank the result starting with the most frequent type.
+select `type`, count(card_id) from card group by `type`;
+#Using the loan table, print the top 10 account_ids based on the sum of all of their loan amounts.
+select account_id, sum(amount) from loan group by account_id order by sum(amount) desc limit 10;
+#In the loan table, retrieve the number of loans issued for each day, before (excl) 930907, ordered by date in descending order.
+select* from loan;
+select `date`, count(loan_id) from loan where `date` < 930907 group by `date` order by `date` desc;
+#In the loan table, for each day in December 1997, count the number of loans issued for each unique loan duration, ordered by date and duration, both in ascending order. You can ignore days without any loans in your output.
+select `date`, count(loan_id), distinct duration from loan where `date` like '9712%' group by duration limit 10;
+#In the trans table, for account_id 396, sum the amount of transactions for each type (VYDAJ = Outgoing, PRIJEM = Incoming). Your output should have the account_id, the type and the sum of amount, named as total_amount. Sort alphabetically by type.
+select account_id, `type`, sum(amount) as total_amount from trans where account_id = 396 group by `type`;
+#From the previous output, translate the values for type to English, rename the column to transaction_type, round total_amount down to an integer
+select account_id, case when `type` = 'prijem' then 'INCOMING' WHEN `type` = 'vydaj' then 'OUTGOING' END as 'transaction_type', floor(sum(amount)) as total_amount from trans where account_id = 396 group by `type`;
+#20. From the previous result, modify your query so that it returns only one row, with a column for incoming amount, outgoing amount and the difference.
+select account_id, concat(`amount`,' ', `amount`) as 'amount', floor(sum(amount)) as total_amount from trans where account_id = 396 group by `type` limit 1;
